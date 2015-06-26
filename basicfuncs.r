3323efabe49b7b6cc3ac230b7ccbec530db37b55
Yoav Bergner <ybergner@gmail.com>
2015-06-26 14:42:15 +0200

    add simulated data function to basicfuncs

--- basicfuncs.r
+++ basicfuncs.r
@@ -1,7 +1,7 @@
 #######################################################
 #	basic sigmoid and logistic functions for IRT models
 #	to use with cfirt code 
-#		Yoav Bergner, 2012
+#		Yoav Bergner, 2015
 #######################################################
 
 sigmoid <- function(z) {
@@ -22,4 +22,40 @@ logistic2pl.vec <- function(sk, d, a) {
 	dmat = matrix(rep(d,length(sk)),nrow=length(sk),byrow=T)
 	amat = matrix(rep(a,length(sk)),nrow=length(sk),byrow=T)	
 	sigmoid(amat * (smat - dmat))
+}
+
+
+# for simulating data with specified item structure
+# and random item parameters
+# by default, the data are multi-unidimensional
+pseudoM2PLdataSI <- function(numStudents, numItems, mu, Sigma) {
+    require(mnormt)
+    dimK <- ncol(Sigma)
+    theta <- rmnorm(numStudents, mu, Sigma)
+   
+	# slopes: item structure provides indices for non-zero slopes
+    itemstructure <- cbind(1:numItems, sample(1:dimK,numItems, replace=T))
+    slopes <- itemstructure*0 
+	slopes[itemstructure] <- rlnorm(numItems,0,0.5) 
+
+	intercepts=runif(numItems,min=-3,max=2) ## difficulty param, uniform random
+		
+	RandomArray=array(runif(numStudents * numItems), dim=c(numStudents, numItems)) # Random between 0 and 1 from uniform distr
+	pseudoscores=array(-1, dim=c(numStudents, numItems)) # initialize
+    
+	# matrix format for intercepts
+	I = intercepts*array(1, dim = c(numItems, numStudents))
+	
+	# matrix format for logit
+	Z = theta%*%t(slopes) + t(I)
+
+	# expected response
+	P = sigmoid(Z)	
+
+	# observed data
+	pseudoscores = ifelse(P > RandomArray, 1, 0)
+	
+	#return the matrix and the parameters used
+	return(list(pseudomatrix=pseudoscores,skills=theta,intercepts=intercepts,slopes=slopes, itemstructure=itemstructure))
+	
 }
